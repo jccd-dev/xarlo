60612dea6e70288b0024086ff68cf34e58905d14
jccd-dev <johncarlo.digay@outlook.com>
2025-02-28 18:47:55 +0800

    added navigation menu with motion animation and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ const Home = () => {
           {/* Main Hero Content */}
           <div className='flex w-full flex-col items-center justify-center gap-6'>
             <div className=''>
-              <h1 className='font-uncage text-[clamp(2rem,12vw,9rem)] tracking-tighter md:leading-[0.95]'>
+              <h1 className='font-uncage text-[clamp(2rem,12vw,10rem)] tracking-tighter md:leading-[0.95]'>
                 <div className='block w-full text-start'>John Carlo</div>
               </h1>
             </div>
-            <div className='absolute left-1/2 -translate-x-1/2 transform'>
-              <div className='font-uncage text-md font-light tracking-normal text-gray-500 md:text-2xl'>
+            <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform whitespace-nowrap text-lg text-neutral-400 sm:top-[48%] md:text-xl'>
+              <div className='text-md font-uncage font-thin tracking-normal text-gray-500 md:text-2xl'>
                 WEB DEVELOPER
               </div>
             </div>
             <div className=''>
-              <h1 className='font-uncage text-[clamp(2rem,12vw,9rem)] tracking-tighter md:leading-[0.95]'>
+              <h1 className='font-uncage text-[clamp(2rem,12vw,10rem)] tracking-tighter md:leading-[0.95]'>
                 <div className='block w-full text-start'>C. Digay</div>
               </h1>
             </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,12 +13,12 @@ const navLinks = [
 export default function Navigation() {
   const [isActive, setIsActive] = useState(false);
   const activeClass =
-    'before:-rotate-45 before:top-[1px] after:rotate-45 after:top-[-1px]';
+    'before:-rotate-45 before:top-[0px] after:rotate-45 after:top-[-1px]';
 
   return (
     <nav className='fixed left-0 right-0 top-0 z-40'>
       <div className='relative mx-auto flex max-w-[1440px] items-center justify-between px-4 md:px-[5%]'>
-        <div className='font-anomaly text-xl font-bold text-foreground'>
+        <div className='font-uncage text-2xl font-semibold text-foreground'>
           XARLO
         </div>
         <div
@@ -28,7 +28,7 @@ export default function Navigation() {
           onClick={() => setIsActive(!isActive)}
         >
           <div
-            className={`w-full before:relative before:top-[5px] before:m-auto before:block before:h-[1px] before:w-2/6 before:bg-current before:transition-transform before:duration-300 before:content-[''] after:relative after:-top-[5px] after:m-auto after:block after:h-[1px] after:w-2/6 after:bg-current after:transition-transform after:duration-300 after:content-[''] ${
+            className={`w-full before:relative before:top-[5px] before:m-auto before:block before:h-[2px] before:w-2/6 before:bg-current before:transition-transform before:duration-300 before:content-[''] after:relative after:-top-[5px] after:m-auto after:block after:h-[2px] after:w-2/6 after:bg-current after:transition-transform after:duration-300 after:content-[''] ${
               isActive ? activeClass : ''
             }`}
           ></div>
@@ -47,10 +47,10 @@ export default function Navigation() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className='relative -top-8 z-20 w-[33rem] max-w-full rounded-md bg-foreground px-5 pb-8 pt-16'
+              className='relative -top-8 z-20 w-[33rem] max-w-full rounded-md bg-foreground px-6 py-16 pb-10'
             >
               <div className='flex flex-col justify-between gap-8'>
-                <div className=''>
+                <div className='flex flex-col gap-4'>
                   {navLinks.map((link, index) => (
                     <motion.div
                       key={link.href}
@@ -61,7 +61,7 @@ export default function Navigation() {
                     >
                       <Link
                         href={link.href}
-                        className='text-4xl font-light text-white transition-colors hover:text-gray-300'
+                        className='font-uncage text-5xl font-semibold text-westar-800 transition-colors hover:text-westar-300'
                         onClick={() => setIsActive(false)}
                       >
                         {link.label}
@@ -70,14 +70,16 @@ export default function Navigation() {
                   ))}
                 </div>
 
-                <div className='mt-auto pt-8'>
-                  <p className='text-sm text-white'>contact@example.com</p>
-                  <p className='text-sm text-white'>@socialhandle</p>
-                  <div className='mt-4'>
-                    <p className='text-sm text-white'>
-                      Freelancer Availability
-                    </p>
-                    <p className='text-sm text-white'>• Limited</p>
+                <div className='gird-cols-1 mt-auto grid pt-8 text-sm text-westar-500 sm:grid-cols-2'>
+                  <div className=''>
+                    <div className=''>johncarlo.digay@outlook.com</div>
+                    <div className=''>+63 917 123 4567</div>
+                  </div>
+                  <div className='flex justify-end'>
+                    <div className='text-start'>
+                      <p className=''>@johncarlo7240</p>
+                      <p className=''>@socialhandle</p>
+                    </div>
                   </div>
                 </div>
               </div>
